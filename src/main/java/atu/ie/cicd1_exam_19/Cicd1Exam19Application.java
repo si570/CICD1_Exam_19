2bb6eeee8d5a2ee798de5e5830bb2fa3efc2a39b
SUBHANIQBAL-STUDENT <G00433839@atu.ie>
2025-11-19 14:10:17 +0000

    Starting files

---
+++ src/main/java/atu/ie/cicd1_exam_19/Cicd1Exam19Application.java
@@ -0,0 +1,13 @@
+package atu.ie.cicd1_exam_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1Exam19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1Exam19Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/cicd1_exam_19/Cicd1Exam19ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.cicd1_exam_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1Exam19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
